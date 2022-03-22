ec04b6a81abefccfdf4e25360b0ec5ba13151812
Max Rydahl Andersen <max.andersen@gmail.com>
2022-03-22 23:44:20 +0100

    update

--- hello.java
+++ hello.java
@@ -6,6 +6,6 @@ import static java.lang.System.*;
 public class hello {
 
     public static void main(String... args) {
-        out.println("Hello World");
+        out.println("Hello Universe");
     }
 }
